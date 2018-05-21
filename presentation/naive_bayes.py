2fac2851180874597f91bdcbbd099024d270a663
Caner Kaya <canerky96@users.noreply.github.com>
2018-05-21 18:29:30 +0300

    rename

--- presentation/naive_bayes.py
+++ presentation/naive_bayes.py
@@ -5,18 +5,14 @@ from time import time
 import nltk
 import pandas as pd
 import numpy as np
-from sklearn.cross_validation import train_test_split
 from sklearn.metrics import classification_report
 import sklearn.metrics
 from pandas import DataFrame,Series
-from sklearn.feature_extraction.text import TfidfVectorizer
-from time import time
 import matplotlib.pyplot as plt
 
 training_time_container={'b_naive_bayes':0,'mn_naive_bayes':0,'random_forest':0,'linear_svm':0}
 prediction_time_container={'b_naive_bayes':0,'mn_naive_bayes':0,'random_forest':0,'linear_svm':0}
 accuracy_container={'b_naive_bayes':0,'mn_naive_bayes':0,'random_forest':0,'linear_svm':0}
-
 
 dataframe = document_token_new.copy()
 dataframe['class'] = dataframe['c_id']
@@ -33,6 +29,20 @@ variables = tfidf_matrix
 labels = dataframe.iloc[:,12].values
 labels = labels.astype('float64')
 
+dataframe_notext = dataframe.iloc[:,1:]
+tfidf_matrix = pd.DataFrame(tfidf_matrix)
+
+for column in list(dataframe_notext.columns.values):
+    tfidf_matrix[str(column)] = 0.0
+
+counter = 0
+for index, row in dataframe_notext.iterrows():
+    for column in list(dataframe_notext.columns.values):
+        tfidf_matrix.at[counter,column] = row[str(column)]
+        
+    counter+=1
+
+
 '''
 variables_train, variables_test, labels_train, labels_test=train_test_split(
         variables, labels, test_size=.3)
@@ -40,7 +50,13 @@ variables_train, variables_test, labels_train, labels_test=train_test_split(
 print('Shape of Training Data: '+str(variables_train.shape))
 print('Shape of Test Data: '+str(variables_test.shape))
 '''
+################################################
+variables = tfidf_matrix.iloc[:,:].values
+labels = dataframe.iloc[:,12].values
+labels = labels.astype('float64')
 
+
+################################################
 from sklearn.naive_bayes import BernoulliNB
 bnb_classifier=BernoulliNB()
 t0=time()
